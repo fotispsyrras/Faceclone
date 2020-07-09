0e8ddcaaa6d11f19b8d44b17075a8d97e0c7dc66
Fotis Psyrras <66555462+fotispsyrras@users.noreply.github.com>
2020-07-09 18:30:31 +0300

    Add files via upload

---
+++ precache-manifest.eaa00a61fb1a744b8b997fa76d1c72e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca326dcad6f1ba219b35cc0bfb28886f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb2f0b02794357c05220",
+    "url": "/static/css/main.c4f7a36c.chunk.css"
+  },
+  {
+    "revision": "78df4633d6b42f4a0131",
+    "url": "/static/js/2.46735fa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.46735fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb2f0b02794357c05220",
+    "url": "/static/js/main.06898a36.chunk.js"
+  },
+  {
+    "revision": "3154f66807a73f30e1c7",
+    "url": "/static/js/runtime-main.36dbbcc8.js"
+  },
+  {
+    "revision": "5411bbeac18e02661efd3eadefb7da77",
+    "url": "/static/media/header.5411bbea.png"
+  }
+]);
